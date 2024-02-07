69c19399ed6623a21f416ee3ebd078e30a8d7b97
Hamza Khalid <hamzakhalid829@gmail.com>
2024-02-07 13:09:02 +0500

    Update HH config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,26 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: "polygon",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygon: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY!],
+    }
+  }
 };
 
 export default config;
